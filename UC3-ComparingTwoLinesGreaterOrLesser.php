d426e02f56b7c6fa3137bfd56305f3d927b4582e
swapnil <sopiswarrinku77@gmail.com>
2022-06-27 12:54:45 -0700

    Added : comparing length of two lines

--- UC3-ComparingTwoLinesGreaterOrLesser.php
+++ UC3-ComparingTwoLinesGreaterOrLesser.php
@@ -1,33 +1,35 @@
 <?php
 
 
-class LineComparisonProgram{
+class LineComparison{
     public $x1;
     public $x2;
     public $y1;
     public $y2;
-    public $x3;
-    public $y3;
-    public $lengthOfline1;
-    public $lengthOfline2;
-   public function checkingEquality(){
+    public $lengthOfline;
+
+   public function userInput(){
     // checking both lines is equal or not
+     $this->x1 = readline("put value of x1 : ");
+     $this->x2 = readline("put value of x2 : ");
+     $this->y1 = readline("put value of y1 : ");
+     $this->y2 = readline("put value of y2 : ");
+     /*$x3 = readline("put value of x3 : ");
+     $y3 = readline("put value of y3 : ");*/
+   }
 
-     $x1 = readline("put value of x1 : ");
-     $x2 = readline("put value of x2 : ");
-     $y1 = readline("put value of y1 : ");
-     $y2 = readline("put value of y2 : ");
-     $x3 = readline("put value of x3 : ");
-     $y3 = readline("put value of y3 : ");
+   public function calLengthOfLine(){
+     $this->lengthOfline = sqrt(($this->x2-$this->x1)**2+($this->y2-$this->y1)**2);
+    //  $lengthOfline2 = sqrt(($x3-$x2)**2+($y3-$y2)**2);
+    return round ($this->lengthOfline,2);
+   }
 
-     $lengthOfline1 = sqrt(($x2-$x1)**2+($y2-$y1)**2);
-     $lengthOfline2 = sqrt(($x3-$x2)**2+($y3-$y2)**2);
-
-    if($lengthOfline1 == $lengthOfline2){
+   public function checkEquality($line1,$line2){
+    if($line1 == $line2){
         echo "both lines are equal";
-    }elseif($lengthOfline1 > $lengthOfline2){
+    }elseif($line1 > $line2){
         echo "line1 is greater that line2";
-    }elseif($lengthOfline1 < $lengthOfline2){
+    }elseif($line1 < $line2){
         echo "line2 is greater that line1";
     }else{
         "not any line is same ";
@@ -36,8 +38,16 @@ class LineComparisonProgram{
    }
 } 
 
-$obj = new LineComparisonProgram();
-$obj->checkingEquality();
+$obj = new LineComparison();
+$obj->userInput();
+$line1 = $obj->calLengthOfLine();
+echo "length of line is : $line1 \n";
+
+$obj->userInput();
+$line2 = $obj->calLengthOfLine();
+echo "length of line is : $line2 \n";
+
+$obj->checkEquality($line1,$line2);
 
 
 ?>

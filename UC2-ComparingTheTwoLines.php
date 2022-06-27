bb97cef78accba7ec36c68bdf138f86114e5398f
swapnil <sopiswarrinku77@gmail.com>
2022-06-27 15:02:07 -0700

    Added : compared two lines are equal or not

--- UC2-ComparingTheTwoLines.php
+++ UC2-ComparingTheTwoLines.php
@@ -1,21 +1,30 @@
 <?php
 
-class LineComparisonProgram{
+class LineComparison{
 
-   public function checkingEquality(){
+    public $x1;
+    public $x2;
+    public $y1;
+    public $y2;
+    public $lengthOfLine;
+   
     // checking both lines is equal or not
+  public function userInput(){
+     $this->x1 = readline("put value of x1 : ");
+     $this->x2 = readline("put value of x2 : ");
+     $this->y1 = readline("put value of y1 : ");
+     $this->y2 = readline("put value of y2 : ");
+    /* $x3 = readline("put value of x3 : ");
+     $y3 = readline("put value of y3 : ");*/
+}
 
-     $x1 = readline("put value of x1 : ");
-     $x2 = readline("put value of x2 : ");
-     $y1 = readline("put value of y1 : ");
-     $y2 = readline("put value of y2 : ");
-     $x3 = readline("put value of x3 : ");
-     $y3 = readline("put value of y3 : ");
+public function calLengthOfLine(){
+     $this->lengthOfLine = sqrt(($this->x2-$this->x1)**2+($this->y2-$this->y1)**2);
+     return round($this->lengthOfLine,2);
+}
 
-     $lengthOfline1 = sqrt(($x2-$x1)**2+($y2-$y1)**2);
-     $lengthOfline2 = sqrt(($x3-$x2)**2+($y3-$y2)**2);
-
-    if($lengthOfline1 == $lengthOfline2){
+public function checkEquality($line1,$line2){
+    if($line1 == $line2){
         echo "both lines are equal";
     }else{
         echo "Both lines are not a equal ";
@@ -24,8 +33,16 @@ class LineComparisonProgram{
    }
 } 
 
-$obj = new LineComparisonProgram();
-$obj->checkingEquality();
+$obj = new LineComparison();
+$obj->userInput();
+$line1 = $obj->calLengthOfLine();
+echo "length of line is : $line1 \n";
+
+$obj->userInput();
+$line2 = $obj->calLengthOfLine();
+echo "length of line is : $line2 \n";
+
+$obj->checkEquality($line1,$line2);
 
 
 
